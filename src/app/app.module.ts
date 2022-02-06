82028bc43653078ff75f8edded42478fec862347
Yago Mascarenhas <yago.mscr@gmail.com>
2022-02-06 18:39:43 -0300

    Dois componentes de alerta criados
    Primeiro exercício do curso de Angular adicionado, criar dois containers (um de sucesso e um de aviso). Estilizados usando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component'
+import { WarningAlert } from './warning_alert/warning_alert.component';
+import { SuccessAlert } from './success_alert/success_alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlert,
+    SuccessAlert
   ],
   imports: [
     BrowserModule
---
+++ src/app/warning_alert/warning_alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "warning-alert",
+    templateUrl: "./warning_alert.component.html"
+})
+export class WarningAlert {
+
+}
