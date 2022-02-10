b92d82d96169cb0548b29d5a94736d2bd172fea5
Yago Mascarenhas <yago.mscr@gmail.com>
2022-02-10 18:23:29 -0300

    Adicionado segundo exercício básico de Angular
    Adicionado exercício que lê um input com nome de usuário e exibe após clique do botão, mas o botão apenas está disponível se o nome de usuário não está vazio.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { updateShorthandPropertyAssignment } from 'typescript';
 
 @Component({
   selector: 'app-servers',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "Nenhum servidor foi criado";
+  userStatus = ""
+  serverName = "TestServer";
+  serverCreated = false;
+  username = "";
+  allowButton = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
+
   }
+
+  allowRenameUser(event: Event) {
+    if ((<HTMLInputElement>event.target).value === "") {
+      this.allowButton = false;
+    } else { 
+      this.allowButton = true;
+    }
+  }
+
+  onRenameUser() {
+    this.userStatus = "O usuário " + this.username + " foi renomeado!"
+    this.username = "";
+    this.allowButton = false;
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "O servidor " + this.serverName + " foi criado";
+  }
+  
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  
 
 }
